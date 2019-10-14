3b8f8b4fc6d5cfdfca149b142ff7cadba6ffae7a
Adrien Brochard <aaa.brochard@gmail.com>
2019-10-14 16:33:29 -0400

    better buffer behavior

--- todoist.el
+++ todoist.el
@@ -368,8 +368,9 @@ P is a prefix argument to select a project."
   (interactive)
   (let ((projects (todoist--get-projects))
          (tasks (todoist--get-tasks)))
-    (with-output-to-temp-buffer todoist-buffer-name
-      (switch-to-buffer todoist-buffer-name)
+    (with-temp-buffer todoist-buffer-name
+      (pop-to-buffer todoist-buffer-name)
+      (delete-region (point-min) (point-max))
       (todoist-mode)
       (insert "#+title: Todoist\n")
       (todoist--insert-heading 1 "Today")
