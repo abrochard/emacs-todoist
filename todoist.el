71d1c74ae60715236883e7f566563c24477b189f
r4dian <r4dian@users.noreply.github.com>
2023-01-19 16:22:30 +0000

    Bump version number to 1.1

--- todoist.el
+++ todoist.el
@@ -19,7 +19,7 @@
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 ;; USA
 
-;; Version: 1.0
+;; Version: 1.1
 ;; Author: Adrien Brochard
 ;; Keywords: todoist task todo comm
 ;; URL: https://github.com/abrochard/emacs-todoist
