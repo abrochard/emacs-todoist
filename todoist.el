59630808afcea627bdd7ee8559aef029e425824c
r4dian <r4dian@users.noreply.github.com>
2023-01-19 15:53:28 +0000

    Update todoist.el

--- todoist.el
+++ todoist.el
@@ -366,9 +366,9 @@ P is a prefix argument to select a project."
   (interactive)
   (let ((task-id (todoist--under-cursor-task-id))
         (content (read-string "Task content: " (org-entry-get nil "ITEM")))
-        (if todoist-use-scheduled-instead-of-deadline
-            (due (read-string "Task due: " (todoist--parse-org-time-string (org-entry-get nil "SCHEDULED"))))
-            (due (read-string "Task due: " (todoist--parse-org-time-string (org-entry-get nil "DEADLINE"))))))
+        (due (if todoist-use-scheduled-instead-of-deadline
+          (read-string "Task due: " (todoist--parse-org-time-string (org-entry-get nil "SCHEDULED")))
+          (read-string "Task due: " (todoist--parse-org-time-string (org-entry-get nil "DEADLINE"))))))
     (todoist--query "POST" (format "/tasks/%s" task-id) `(("content" . ,content) ("due_string" . ,due)))
     (todoist)))
 
